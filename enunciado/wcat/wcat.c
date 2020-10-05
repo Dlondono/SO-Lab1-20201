caf1a654d9dffd475f16351c32f66d8db79aa9bc
Dlondono <david020897@gmail.com>
2020-10-05 07:28:08 -0500

    lab 1

---
+++ enunciado/wcat/wcat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *fp;
+    char buff[1024];
+
+    for (size_t i = 1; i < argc; i++)
+    {
+        if ((fp = fopen(argv[i], "r")) == NULL)
+        {
+            printf("wcat: cannot open file\n");
+            exit(1);
+        }
+
+        while (fgets(buff, 1024, fp)){
+            printf("%s", buff);
+	}
+	fclose(fp);
+}
+    return 0;
+}
---
+++ enunciado/wget/wgrep.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+
+    FILE *fp;
+    char buff[1024];
+    char *word = argv[1]; 
+    if (argc < 2)
+    {
+        printf("wgrep: searchterm [file ...]\n");
+        exit(1);
+    }
+    if (argc < 3)
+    {
+            fp = stdin;
+            fscanf(fp, "%s", buff);
+            while (fgets(buff, 1024, fp))
+            {
+                if (strstr(buff, word) != NULL)
+                {
+                    printf("%s", buff);
+                }
+            }
+    }
+
+    if (strstr(" ", word) == NULL)
+    {
+        for (size_t i = 2; i < argc; i++)
+        {
+            fp = fopen(argv[i], "r");
+            if (fp == NULL)
+            {
+                printf("wgrep: cannot open file\n");
+                exit(1);
+            }
+
+            while (fgets(buff, 1024, fp))
+            {
+                if (strstr(buff, word) != NULL)
+                {
+                    printf("%s", buff);
+                }
+            }
+            fclose(fp);
+        }
+    }
+
+    return 0;
+}
